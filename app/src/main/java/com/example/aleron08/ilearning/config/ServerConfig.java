67e4f312b2b5eb978f4fa7bf641f21928d6c7727
aleron08 <aleron08@163.com>
2019-03-14 17:27:50 +0800

    1.新增ServerConfig类

---
+++ app/src/main/java/com/example/aleron08/ilearning/config/ServerConfig.java
@@ -0,0 +1,12 @@
+package com.example.aleron08.ilearning.config;
+
+/**
+ * Created by aleron08 on 2019/3/14.
+ */
+
+public class ServerConfig {
+
+    public static final String serverIP = "40.121.148.51";
+
+    public static final String port = "8080";
+}
--- app/src/main/java/com/example/aleron08/ilearning/view/activity/NewTeachActivity.java
+++ app/src/main/java/com/example/aleron08/ilearning/view/activity/NewTeachActivity.java
@@ -2,14 +2,43 @@ package com.example.aleron08.ilearning.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.aleron08.ilearning.R;
+import com.example.aleron08.ilearning.util.ImmersiveStatusBar;
 
-public class NewTeachActivity extends AppCompatActivity {
+public class NewTeachActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ImageView ivBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_teach);
+        LinearLayout linearLayout = findViewById(R.id.ll_new_teach_actionbar);
+        ImmersiveStatusBar.initStateInActivity(this,linearLayout);
+        initToolBar();
+        initViewPager();
+        initTabLayout();
+    }
+
+    public void initToolBar(){
+        ivBack = findViewById(R.id.iv_new_teach_back);
+        ivBack.setOnClickListener(this);
+    }
+
+    public void initViewPager(){
+
+    }
+
+    public void initTabLayout(){
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
